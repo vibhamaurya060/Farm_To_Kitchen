6f09304d6dc69916dbc1e53524ac877faa105664
Vibha Maurya <vibhamaurya060@gmail.com>
2025-04-13 10:18:19 +0530

    add

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -161,7 +161,6 @@ const Home = () => {
                                 </div>
                                 <div className="product-info">
                                     <h3 className="product-name">{product.name}</h3>
-                                    <p className="product-price">{`$ ${product.price.toFixed(2)}`}</p>
                                     <div className="product-buttons">
                                         <button
                                             className="view-details-button"
